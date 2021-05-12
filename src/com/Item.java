cff557731fdc9bcd13e0dfd66cf4bc429ef70e4f
h_mandakini <himasha.mandakini@gmail.com>
2021-05-12 22:44:28 +0530

    update function

--- src/com/Item.java
+++ src/com/Item.java
@@ -130,10 +130,59 @@ public class Item {
 	
 	
 	
-	public String readItems() 
-	 { 
+//	public String readItems() 
+//	 { 
+//	 String output = ""; 
+//	 try
+//	 { 
+//	 Connection con = connect(); 
+//	 if (con == null) 
+//	 { 
+//	 return "Error while connecting to the database for reading."; 
+//	 } 
+//	 // Prepare the html table to be displayed
+//	 output = "<table border='1'><tr><th>Item Code</th> <th>Item Name</th><th>Item Price</th>"
+//	 + "<th>Item Description</th> <th>Update</th><th>Remove</th></tr>"; 
+//	 String query = "select * from items"; 
+//	 Statement stmt = con.createStatement(); 
+//	 ResultSet rs = stmt.executeQuery(query); 
+//	 // iterate through the rows in the result set
+//	 while (rs.next()) 
+//	 { 
+//	 String itemID = Integer.toString(rs.getInt("itemID")); 
+//	 String itemCode = rs.getString("itemCode"); 
+//	String itemName = rs.getString("itemName");
+//	 String itemPrice = Double.toString( 
+//	 rs.getDouble("itemPrice")); 
+//	 String itemDesc = rs.getString("itemDesc"); 
+//	 // Add into the html table
+//	 output += "<tr><td><input id='hidItemIDUpdate' name='hidItemIDUpdate' type='hidden' value='" + itemID
+//	 + "'>" + itemCode + "</td>"; 
+//	 output += "<td>" + itemName + "</td>"; 
+//	 output += "<td>" + itemPrice + "</td>"; 
+//	 output += "<td>" + itemDesc + "</td>"; 
+//	 // buttons
+//	output += "<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary'></td>"
+//	 + "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-itemid='"
+//	 + itemID + "'>" + "</td></tr>"; 
+//	 } 
+//	 con.close(); 
+//	 // Complete the html table
+//	 output += "</table>"; 
+//	 } 
+//	 catch (Exception e) 
+//	 { 
+//	 output = "Error while reading the items."; 
+//	 System.err.println(e.getMessage()); 
+//	 } 
+//	 return output; 
+//	 }
+	
+	
+	public String readItems()
+	{ 
 	 String output = ""; 
-	 try
+	try
 	 { 
 	 Connection con = connect(); 
 	 if (con == null) 
@@ -141,8 +190,10 @@ public class Item {
 	 return "Error while connecting to the database for reading."; 
 	 } 
 	 // Prepare the html table to be displayed
-	 output = "<table border='1'><tr><th>Item Code</th> <th>Item Name</th><th>Item Price</th>"
-	 + "<th>Item Description</th> <th>Update</th><th>Remove</th></tr>"; 
+	 output = "<table border='1'><tr><th>Item Code</th>" 
+	 + "<th>Item Name</th><th>Item Price</th>"
+	 + "<th>Item Description</th>" 
+	 + "<th>Update</th><th>Remove</th></tr>"; 
 	 String query = "select * from items"; 
 	 Statement stmt = con.createStatement(); 
 	 ResultSet rs = stmt.executeQuery(query); 
@@ -151,32 +202,31 @@ public class Item {
 	 { 
 	 String itemID = Integer.toString(rs.getInt("itemID")); 
 	 String itemCode = rs.getString("itemCode"); 
-	String itemName = rs.getString("itemName");
-	 String itemPrice = Double.toString( 
-	 rs.getDouble("itemPrice")); 
+	 String itemName = rs.getString("itemName"); 
+	 String itemPrice = Double.toString(rs.getDouble("itemPrice")); 
 	 String itemDesc = rs.getString("itemDesc"); 
-	 // Add into the html table
-	 output += "<tr><td><input id='hidItemIDUpdate' name='hidItemIDUpdate' type='hidden' value='" + itemID
-	 + "'>" + itemCode + "</td>"; 
+	// Add into the html table
+	 output += "<tr><td>" + itemCode + "</td>"; 
 	 output += "<td>" + itemName + "</td>"; 
 	 output += "<td>" + itemPrice + "</td>"; 
 	 output += "<td>" + itemDesc + "</td>"; 
-	 // buttons
-	output += "<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary'></td>"
-	 + "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-itemid='"
-	 + itemID + "'>" + "</td></tr>"; 
+	// buttons
+	output += "<td><input name='btnUpdate' type='button' value='Update' "
+	+ "class='btnUpdate btn btn-secondary' data-itemid='" + itemID + "'></td>"
+	+ "<td><input name='btnRemove' type='button' value='Remove' "
+	+ "class='btnRemove btn btn-danger' data-itemid='" + itemID + "'></td></tr>"; 
 	 } 
 	 con.close(); 
 	 // Complete the html table
 	 output += "</table>"; 
 	 } 
-	 catch (Exception e) 
+	catch (Exception e) 
 	 { 
 	 output = "Error while reading the items."; 
 	 System.err.println(e.getMessage()); 
 	 } 
-	 return output; 
-	 }
+	return output; 
+	}
 	
 	
 	
